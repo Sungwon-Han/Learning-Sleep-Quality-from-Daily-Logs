d8d8f0e4c6631a031dc948904d69f72cfb0112ec
hsuchengmath <31360529+hsuchengmath@users.noreply.github.com>
2019-02-07 22:25:30 +0800

    Update main.py

--- GAIN/main.py
+++ GAIN/main.py
@@ -1,8 +1,8 @@
+import json
 import argparse
+
 import pandas as pd
-
 from preprocess import *
-
 from GAIN import GAIN
 
 def Argument():
@@ -18,40 +18,46 @@ if __name__ == '__main__':
 
     args = Argument()
 
+    json_path = '....../parameter.json'
+    
+    with open(json_path,'r') as file_object:
+        parameter = json.load(file_object)
 
-    epouh = 20000
-    file_path = 'xxxx/sleeps_ORIGINAL.csv'
+    file_path = parameter['GAIN']['file_path']
+    save_path = parameter['GAIN']['save_path']
+    epoch = parameter['GAIN']['epoch']
+   
   
     ##loading data
     sleep_activity_data_csv = pd.read_csv(file_path)
     sleep_activity_data_csv = Correct(sleep_activity_data_csv)
     
     ##user_Id
-    user_Id = list(set(sleep_activity_data_csv["userId"]))  #%
+    user_Id = list(set(sleep_activity_data_csv["userId"]))  
     user_Id_date = sleep_activity_data_csv[['userId','month','date']]
 
     ##feature name
-    feature_name = list(sleep_activity_data_csv.columns)[3:] #%
+    feature_name = list(sleep_activity_data_csv.columns)[3:] 
 
     ##Calculating Max and Min
-    max_ = sleep_activity_data_csv[sleep_activity_data_csv.columns[3:]].max()  #%
-    min_ = sleep_activity_data_csv[sleep_activity_data_csv.columns[3:]].min()  #%
+    max_ = sleep_activity_data_csv[sleep_activity_data_csv.columns[3:]].max()  
+    min_ = sleep_activity_data_csv[sleep_activity_data_csv.columns[3:]].min()  
     
     ##FillNa with 0.8
     sleep_activity_data = sleep_activity_data_csv.fillna(0.8)
 
     ##Standard
     sleep_activity_data_standard = (sleep_activity_data[sleep_activity_data.columns[3:]]-min_)/(max_-min_) 
-    sleep_activity_data_standard =  pd.concat([user_Id_date,sleep_activity_data_standard],axis=1) #%
+    sleep_activity_data_standard =  pd.concat([user_Id_date,sleep_activity_data_standard],axis=1) 
 
     ##Storing user data to dictionary
     dict_user_data = Dict_user_data(sleep_activity_data_standard)
     
     ##Transforming user_data into window format and record the number of window in each user
-    dict_window,window_number = Dict_window(dict_user_data,user_Id_date)  #%
+    dict_window,window_number = Dict_window(dict_user_data,user_Id_date)  
     
     ##transfer dict_window into window 
-    window = Window(dict_window,user_Id_date) #%
+    window = Window(dict_window,user_Id_date) 
     
     ##setting mask matrix and can set the number of missong data
     mask_dataframe = Mask_dataframe(sleep_activity_data_csv)
@@ -64,7 +70,8 @@ if __name__ == '__main__':
     impute_data_standard,impute_data = Delete_window(window_number,generate_window,user_Id,feature_name,max_,min_,user_Id_date)
     
     ##Saving
-    impute_data_standard.to_csv(input_save_path)
+    impute_data.to_csv(save_path)
+    
     
     '''
     impute_data is a dataframe withod min-max normalization
